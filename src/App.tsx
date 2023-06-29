6572db7657e6efa6c1f7db60df587afae2637cdd
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-06-28 21:23:52 -0300

    Install and test TailwindCss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import './styles/global.css';
+import { Habit } from "./components/Habit";
+
 function App() {
   
 
   return (
     
-      <h1>Hello World</h1>
+      <div>
+        <Habit completed={7}/>
+        <Habit completed={6}/>
+        <Habit completed={5}/>
+        <Habit completed={4}/>
+        <Habit completed={3}/>
+        <Habit completed={2}/>
+        <Habit completed={1}/>
+      </div>
   )
 }
 
