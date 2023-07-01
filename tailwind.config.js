86d2dbbed173dcb105c4e720f1d473cec30bd414
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-06-30 23:28:16 -0300

    create Components and tests tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ export default {
   ],
   theme: {
     extend: {},
+      colors: {
+        background: '#09090A'
+      },
+
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))'
+      }
   },
   plugins: [],
 }
