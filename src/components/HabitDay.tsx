d8d3f56d4b44b9df882656dcf8d7d15020eff3e8
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-07-03 20:09:29 -0300

    React Aleatory (colors) HabitDays

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,10 +1,30 @@
 import * as Popover from "@radix-ui/react-popover";
 import { ProgressBar } from "./ProgressBar";
+import clsx from "clsx";
 
-export function HabitDay() {
+
+
+interface HabitDayProps {
+  amount: number
+  completed: number
+}
+
+export function HabitDay({ completed, amount } : HabitDayProps) {
+  
+  const completedPercentage = Math.round((completed / amount) * 100) ;
+
+
   return (
     <Popover.Root>
-      <Popover.Trigger className="w-10 h-10 bg-zinc border-2 border-zincb rounded-lg"></Popover.Trigger>
+      <Popover.Trigger className={clsx('w-10 h-10 bg-zinc border-2 border-zincb rounded-lg', {
+        'bg-violet600 border border-violet600b': completedPercentage >= 80,
+        'bg-violet700 border border-violet700b': completedPercentage >= 60 && completedPercentage < 80,
+        'bg-violet800 border border-violet600': completedPercentage >= 40 && completedPercentage < 60,
+        'bg-violet900 border border-violet700': completedPercentage >= 20 && completedPercentage < 40,
+        'bg-violet1000 border border-violet800': completedPercentage > 0 && completedPercentage < 20,
+        'bg-zinc border border-zincb': completedPercentage === 0,
+        
+      })}></Popover.Trigger>
 
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc flex flex-col ">
@@ -12,7 +32,7 @@ export function HabitDay() {
           <span className="mt-1 font-extrabold leading-tight text-3xl">
             03/07
           </span>
-          <ProgressBar progress={40} />
+          <ProgressBar progress={completedPercentage} />
           <Popover.Arrow height={6} width={12} className="fill-zinc" />
         </Popover.Content>
       </Popover.Portal>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,13 @@ export default {
         background: '#09090A',
         zinc: '#18181B',
         zincb: '#070808',
+        violet600: '#8B5CF6',
+        violet600b: '#C4B5FD',
         violet700: '#7C3AED',
+        violet700b: '#A78BFA',
+        violet800: '#6D28D9',
+        violet900: '#5B21B6',
+        violet1000: '#4C1D95',
         zinc400: '#4F4F4F',
         green600: '#00FF7F',
         green500: '#7FFFD4'
